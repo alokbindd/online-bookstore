663fa0ad582caad2d12876ead3b5d24c370abe3f
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-20 17:19:35 +0530

    Added admin configurations for Category and Book models, including list displays and prepopulated fields.

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Book
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('category_name',)}
+    list_display = ('category_name','slug')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('category','title','slug','isbn','author','price','publication_date','stock')
+    ordering = ('-publication_date',)
+
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
