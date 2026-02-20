3b9c22813c8928432a72aad4c7a4c1d174328803
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-20 20:08:26 +0530

    Add JWT authentication endpoints for token management in the API

--- online_bookstore/urls.py
+++ online_bookstore/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 
 urlpatterns = [
@@ -28,6 +29,8 @@ urlpatterns = [
     # path('books/',include('books.urls')),
     # path('carts/',include('carts.urls')),
     # path('orders/',include('orders.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
