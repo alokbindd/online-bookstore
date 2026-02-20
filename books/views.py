99d642b8c4eb551728330db6a0243fb5a2dd15d2
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-20 23:44:21 +0530

    Add BookView and BookDetailView for listing and managing books in the API

--- books/views.py
+++ books/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import BookSerializer
+from .models import Book
 # Create your views here.
+
+class BookView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+    lookup_field = 'slug'
