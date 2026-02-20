1e28e12d480541fcfe457f35e5e0d50b1f788ee7
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-20 20:07:47 +0530

    Add UserProfile model with fields for user details and initial migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True)
+    address_line_2 = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    pincode = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
