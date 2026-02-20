11f45f34af98df7dfaaa388787f7c234e68b2ff8
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-20 17:05:08 +0530

    added constraints for book model

--- books/models.py
+++ books/models.py
@@ -14,17 +14,26 @@ class Category(models.Model):
         return self.category_name
 
 class Book(models.Model):
-    category    = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='books')
-    cover_image = models.ImageField(upload_to='books/cover_image/', blank=True, null=True)
-    title       = models.CharField(max_length=225, unique=True)
-    slug        = models.SlugField(max_length=225, unique=True)
-    isbn        = models.CharField(max_length=30, unique=True)
-    description = models.TextField(blank=True)
-    author      = models.CharField(max_length=100)
-    price       = models.DecimalField(max_digits=10, decimal_places=2)
-    stock       = models.IntegerField()
-    created_at  = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now=True)
+    category            = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='books')
+    cover_image         = models.ImageField(upload_to='books/cover_image/', blank=True, null=True)
+    title               = models.CharField(max_length=225, unique=True)
+    slug                = models.SlugField(max_length=225, unique=True)
+    isbn                = models.CharField(max_length=30, unique=True)
+    description         = models.TextField(blank=True)
+    author              = models.CharField(max_length=100)
+    price               = models.DecimalField(max_digits=10, decimal_places=2)
+    publication_date    = models.DateField(null=True, blank=True)
+    stock               = models.IntegerField()
+    created_at          = models.DateTimeField(auto_now_add=True)
+    updated_at          = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title','category','author'],
+                name='unique_book_per_category_author'
+            )
+        ]
 
     def __str__(self):
         return self.title
