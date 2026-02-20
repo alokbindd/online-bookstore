899bcb4de16aed7eb59b82695f2293805c2809a2
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-20 17:19:51 +0530

    Add Cart and CartItem models with relationships and constraints; created initial migrations for database schema.

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from books.models import Book
 
 # Create your models here.
+class Cart(models.Model):
+    user        = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    session_id  = models.CharField(max_length=225, null=True, blank=True, unique=True)
+    date_added  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.user:
+            return f'Cart - {self.user.username}'
+        return f'Cart - {self.session_id}'
+    
+class CartItem(models.Model):
+    cart        = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    book        = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity    = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['cart','book'],
+                name='unique_book_cart'
+            )
+        ]
+        
+    def __str__(self):
+        return f'{self.book.title} ({self.quantity})'
